263b152a6325a1c152b89c7b18532a05d0f064dc
Mohsen Afshari <afshari.mvc@gmail.com>
2026-07-01 10:47:14 +0200

    Scaffold safemon-gui-csharp: WPF + xUnit project structure

---
+++ safemon/tools/safemon-gui-csharp/SafemonGui/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SafemonGui;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
